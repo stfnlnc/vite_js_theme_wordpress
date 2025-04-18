595fb70db9e830279058d00de62a647fa835ebd2
Stefan <stefan@studiokhi.com>
2025-04-18 21:10:54 +0200

    add if dist exists

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function vitejs_assets(): void
     if (WP_DEBUG) {
         echo '<script type="module" src="http://localhost:5173/@vite/client"></script>';
         echo '<script type="module" src="http://localhost:5173/src/main.js"></script>';
-    } else {
+    } else if (is_dir(get_theme_file_path() . '/dist')) {
         $data = file_get_contents(get_theme_file_path() . '/dist/.vite/manifest.json');
         $manifest = json_decode($data, true)['index.html'];
         wp_enqueue_script('vitejs', get_template_directory_uri() . '/dist/' . $manifest['file'], [], 1, true);
